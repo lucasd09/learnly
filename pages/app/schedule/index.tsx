a38d8c0fb72e49f10350091c10a8fbe4345587be
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2023-04-17 21:41:24 -0300

    VEM LOUVANDO Ó

---
+++ pages/app/schedule/index.tsx
@@ -0,0 +1,17 @@
+import TemplateLayout from '@/layouts/templateLayout';
+import { NextPageWithLayout } from '@/types/NextPageWithLayout';
+import React, { ReactElement } from 'react';
+
+const Schedule: NextPageWithLayout = () => {
+    return ( 
+      <div>
+        <h1>Schedule</h1>
+      </div>
+    );
+  };
+  
+  Schedule.getLayout = function getLayout(page: ReactElement) {
+    return <TemplateLayout>{page}</TemplateLayout>;
+  };
+  
+  export default Schedule;
