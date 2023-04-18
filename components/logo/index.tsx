b800c421e7d843fec79eaedfa17709a2c65dcbee
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2023-04-18 20:50:07 -0300

    correção para deploy

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import logo from "@/public/frame.svg";
+import logo from "../../Frame.svg";
 import { wrapper } from "./styles.css";
 
 const Logo: React.FC = () => {
