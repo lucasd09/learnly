57a4f985cc0b33c5c9bc4c1292cbde582728aff2
Miguel Francisco Bossoni Barreto <48730491+gitguel@users.noreply.github.com>
2023-05-12 22:01:52 -0300

    tomara que não pegue muito fogo

---
+++ pages/app/subjects/styles.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css";
+
+export const content = style({
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    width: "100%",
+});
---
+++ types/subjectCardProps.ts
@@ -0,0 +1,3 @@
+interface SubjectCardProps {
+    name: string;
+}
